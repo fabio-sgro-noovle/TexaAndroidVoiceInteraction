39ef8b10002872df7f22cd812707cc768e453ff1
Fabio Sgro <fabio.sgro@noovle.it>
2015-02-13 16:15:26 +0100

    import iniziale

--- app/src/main/java/it/noovle/android/texaandroidvoiceinteraction/MainActivity.java
+++ app/src/main/java/it/noovle/android/texaandroidvoiceinteraction/MainActivity.java
@@ -1,17 +1,102 @@
 package it.noovle.android.texaandroidvoiceinteraction;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+    private ImageButton btnSpeak;
+    private TextView txtSpeechInput;
+
+    private boolean test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtSpeechInput = (TextView) findViewById(R.id.textViewResult);
+        btnSpeak = (ImageButton) findViewById(R.id.buttonInteraction);
+
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                promptSpeechInput();
+            }
+        });
+
+    }
+
+    private void promptSpeechInput() {
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "PARLA");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    "TTS NON SUPPORTATO",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String risultato = " ";
+                    int i = 1;
+                    for (String s : result) {
+                        if (i == 1) {
+                            verificaComando(s);
+                        }
+                        risultato = risultato + i + " --> " + s + " \n ";
+                        i++;
+                    }
+                    txtSpeechInput.setText(risultato);
+                }
+                break;
+            }
+
+        }
+    }
+
+    private void verificaComando(String comando) {
+        if (comando.equalsIgnoreCase("cambia colore")) {
+            cambiaColoreBackgroud();
+        }
+    }
+
+    private void cambiaColoreBackgroud() {
+        Random rnd = new Random();
+        int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+        txtSpeechInput.setBackgroundColor(color);
     }
 
 
